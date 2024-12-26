099d7cd7d6f4b59c398c3ade86cb380a61a365dd
Alexander Schilling <dignityinside@protonmail.com>
2024-12-26 10:57:45 +0100

    Add api config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,7 +8,7 @@ export default defineNuxtConfig({
   ],
   runtimeConfig: {
     // The private keys which are only available server-side
-    apiSecret: '123',
+    apiBase: 'https://localhost:3001',
     // Keys within public are also exposed client-side
     public: {
       name: 'Alex Spielwelt',
